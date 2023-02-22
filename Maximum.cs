3b8f4d125d02e2bece0f4f9da6a6c56fb3795e89
riya more <you@example.com>
2023-02-22 09:40:23 +0530

    Generics Problem

---
+++ Maximum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Maximum
+    {
+        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if(firstvalue.CompareTo (secondvalue)>0 && firstvalue.CompareTo(thirdvalue)>0 ||
+                firstvalue.CompareTo(firstvalue)>=0 && secondvalue.CompareTo(thirdvalue)>0 ||
+                secondvalue.CompareTo(firstvalue)>0 && firstvalue.CompareTo(thirdvalue)>=0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            
+                {
+                    return secondvalue;
+                }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Generics;
+
+public static void Main(string[] args)
+{
+    int[] intArray = { 1, 2, 3, 4, 5, };
+    double[] doubleArray = { 1.1, 2.2, 3.3 };
+    char[] charArray = { 'H', 'E', 'l', 'l' };
+
+    PrintArray<int> intAraay =new PrintArray<int>(intArray);
+    intPrintArray.toPrint();
+    PrintArray
+    doublePrintArray
+
+}
+
+
+
